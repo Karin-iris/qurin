8a36c95f1ecce93bfabe7622add2a4250e0f7bd1
Akira Yamanaka <aigrec.system@gmail.com>
2023-03-29 16:20:20 +0900

    翻訳ファイル、ロケーションの確認

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuestionDraftController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::controller(QuestionDraftController::class)->group(function () {
+        Route::get('/questions/{id}', 'edit')->name('question.edit');
+        Route::post('/questions', 'store');
+    });
 });
 
 require __DIR__.'/auth.php';
