d9127958d0504f0e8dac8f8e70d2cd786e9ef8d0
Akira Yamanaka <aigrec.system@gmail.com>
2023-05-28 23:05:05 +0900

    書き出し時改行の除去

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -177,13 +177,13 @@ class QuestionController extends Controller
                     '',
                     '',
                     'true',
-                    $question->correct_choice,
+                    str_replace(array("\r\n", "\r", "\n"), $question->correct_choice),
                     'true',
-                    $question->wrong_choice_1,
+                    str_replace(array("\r\n", "\r", "\n"),$question->wrong_choice_1),
                     'false',
-                    $question->wrong_choice_2,
+                    str_replace(array("\r\n", "\r", "\n"),$question->wrong_choice_2),
                     'false',
-                    $question->wrong_choice_3,
+                    str_replace(array("\r\n", "\r", "\n"),$question->wrong_choice_3),
                     'false'
                 ];
 
