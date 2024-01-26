2ddd028ff91c3f1c51cd0bfeb28ef66873ca1c67
Karin Yamanaka <yamanaka@primeforce.co.jp>
2024-01-26 09:42:06 +0900

    サイトごと設定

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,6 @@
-@if env('APP_COM_NAME')=="dlp"
+@if (env('APP_COM_NAME')=="dlp")
 <img src="{{ asset('images/logo_dlp.png')}}" {{ $attributes }}>
 @endif
-@if env('APP_COM_NAME')=="tc"
+@if (env('APP_COM_NAME')=="tc")
 <img src="{{ asset('images/logo_tc.png')}}" {{ $attributes }}>
 @endif
