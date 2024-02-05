a8f36e957fe913f4491a3a8ed52bb21a414bcd9a
Karin Yamanaka <yamanaka@primeforce.co.jp>
2024-02-05 14:26:41 +0900

    初期データ修正

--- database/seeders/TrSeeder.php
+++ database/seeders/TrSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use App\Models\CategoryPrimary;
 use App\Models\CategorySecondary;
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
