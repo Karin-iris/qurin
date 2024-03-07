da2aaf70c7dbc67da16fe0c242c381275bc49dd2
Karin Yamanaka <yamanaka@primeforce.co.jp>
2024-03-07 15:41:01 +0900

    解答インポート機能

--- app/UseCases/QuestionUseCase.php
+++ app/UseCases/QuestionUseCase.php
@@ -607,7 +607,7 @@ class QuestionUseCase extends UseCase
             'Student_id'
         );
         foreach ($questions as $question) {
-            $columns[] = sprintf("%05d",$question->order);
+            $columns[] = sprintf("%05d",$question->qurstion_id);
         }
         $title = "結果";
         $headers = [
