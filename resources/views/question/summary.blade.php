92eda8854ccbd2dd3c241544a9cdcdf610ba15a0
Karin Yamanaka <yamanaka@primeforce.co.jp>
2024-03-09 16:54:49 +0900

    修正

--- resources/views/question/summary.blade.php
+++ resources/views/question/summary.blade.php
@@ -2,7 +2,7 @@
     @vite(['resources/js/mySectionTable.js'])
 @endsection
 
-<x-app-layout>
+<x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Sections') }}
@@ -111,4 +111,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-admin-layout>
